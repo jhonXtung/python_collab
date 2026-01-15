b9e4c6b69b9ce0822140b2ab9b878f663c6807ec
mengor <roy.aninon@urios.edu.ph>
2026-01-15 08:38:13 +0000

    Initial upload of main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+def main():
+    print("Welcome to our Group Project!")
+
+    # -----------------------------------------
+    # CONTRIBUTOR 1 (You - The Team Leader)
+    # -----------------------------------------
+    print("Leader: Project initialized by Roy Aninon")
+
+    # -----------------------------------------
+    # CONTRIBUTOR 2 (Classmate A)
+    # -----------------------------------------
+    # TODO: Classmate A, please uncomment the line below and add your function
+    # print("Contributor 2: [Name] says hello!")
+
+    # -----------------------------------------
+    # CONTRIBUTOR 3 (Classmate B)
+    # -----------------------------------------
+    # TODO: Classmate B, please uncomment the line below and add your function
+    # print("Contributor 3: [Name] likes Python!")
+
+    # -----------------------------------------
+    # CONTRIBUTOR 4 (Classmate C)
+    # -----------------------------------------
+    # TODO: Classmate C, please uncomment the line below and add your function
+    # print("Contributor 4: [Name] is here!")
+
+if __name__ == "__main__":
+    main()
